1f288811c33e4d6add6606c0d0924285d0261c88
Steven <tedjasuryastevenjonathan@gmail.com>
2022-07-28 00:35:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import BodyComponent from './component/BodyComponent';
+import BodyComponenentFunc from './component/BodyComponenetsfunc.js';
+
+import Header from './component/HeaderComponenents.js'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
+  // const enigma = 'Enigmacamp'
+  // const name = <h1> Hello  {enigma}</h1>
+
+  // function sayHello(name) {
+  //   return `Hello ${enigma}`
+    
+  // }
+
+  // const buttonText = {
+  //   text: 'Kirim Message'
+  // }
+
+  // const buttonStyle = {
+  //   color: 'red',
+  //   fontSize: '10px'
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BodyComponent/>
+      <BodyComponenentFunc/>
+      <Header title = "Judul Test"/>
+      <Header />
+      {/* pake prop types */}
+      <Header title = {1}/>
+      {/* {name}
+      <h1 class = "text-title"> {sayHello(enigma)} </h1>
+      <h1 class = "text-title"> {sayHello(enigma)} </h1> */}
+
+      <div>
+        <h1 class = "text-title"> {sayHello(enigma)} </h1>
+        <h1 class = "text-title"> {sayHello(enigma)} </h1>
+      </div>
+
+    
+
+      {/* <button style = {buttonStyle}> {buttonText.text} </button> */}
+      {/* <button class = "button-name"> {buttonText.text} </button>
+      <label htmlFor='name'> Enter Name </label> */}
+    </>
   );
 }
 
---
+++ src/component/BodyComponenetsfunc.js
@@ -0,0 +1,12 @@
+function BodyComponenentFunc() {
+
+    return (
+        <>
+        <h1> Body Component Function </h1>
+        </>
+    
+    )      
+    
+}
+
+export default BodyComponenentFunc
